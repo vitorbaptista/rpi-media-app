796cfad4c92e086aa954f3547a1d8b53392455ff
Vitor Baptista <vitor@vitorbaptista.com>
2025-05-15 10:15:56 -0300

    Tenta novamente caso tenha algum problema tocando a Sessão da Tarde
    As vezes o catt não funciona de primeira, tocando o vídeo e fechando
    logo depois. Nesta mudança, tentamos uma força bruta -- tentando
    novamente até 5x se a duração da execução do catt for menor que 1
    minuto (que é muito menor do que qualquer vídeo).

--- play_sessao_da_tarde.py
+++ play_sessao_da_tarde.py
@@ -6,6 +6,7 @@ import logging
 import glob
 import subprocess
 import datetime
+import time
 
 # Set up logging
 logging.basicConfig(
@@ -29,6 +30,27 @@ def _get_video_path():
     return video_path
 
 
+def _cast_with_retry_if_finished_too_quickly(
+    video_path, min_execution_duration=60, max_attempts=5
+):
+    attempts = 0
+    while attempts < max_attempts:
+        attempts += 1
+        start_time = time.time()
+        subprocess.run(["catt", "cast", "--block", video_path], check=True)
+        execution_duration = time.time() - start_time
+
+        if execution_duration >= min_execution_duration:
+            return True
+        elif attempts >= max_attempts:
+            logger.info(f"Reached maximum of {max_attempts} attempts.")
+            return False
+        else:
+            logger.info(
+                f"Execution time was {execution_duration:.2f} seconds, below {min_execution_duration} seconds threshold. Running again (attempt {attempts}/{max_attempts})."
+            )
+
+
 def main():
     try:
         video_path = _get_video_path()
@@ -37,9 +59,11 @@ def main():
             return 1
 
         logger.info(f"Playing video: {video_path}")
-        subprocess.run(["catt", "cast", "--block", video_path], check=True)
-        logger.info("Playback completed successfully")
 
+        if not _cast_with_retry_if_finished_too_quickly(video_path):
+            return 1
+
+        logger.info("Playback completed successfully")
         return 0
 
     except Exception as e:
